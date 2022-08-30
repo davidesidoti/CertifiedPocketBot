594d27a5d550295b6113420fb034863479dfce7e
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2022-08-30 11:04:10 +0200

    First Commit
    Main commit of the bot file and creation of the requirements.txt file.

--- __main__.py
+++ __main__.py
@@ -1,2 +1,25 @@
+from dotenv import load_dotenv
 import os
 import discord
+from discord.ext import commands
+
+load_dotenv()
+# Get the API token from the .env file.
+DISCORD_TOKEN = os.getenv('discord_token')
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+bot.remove_command('help')
+
+
+@bot.event
+async def on_ready():
+    for guild in bot.guilds:
+        print('Active in {}\n Member Count : {}'.format(
+            guild.name, guild.member_count))
+
+
+if __name__ == "__main__":
+    bot.run(DISCORD_TOKEN)
