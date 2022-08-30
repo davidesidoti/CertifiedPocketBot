dff4cb53276a965fc27aabc5ab3ae7b01e2729a0
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2022-08-30 11:36:05 +0200

    Created ping command

--- __main__.py
+++ __main__.py
@@ -7,11 +7,29 @@ load_dotenv()
 # Get the API token from the .env file.
 DISCORD_TOKEN = os.getenv('discord_token')
 
+# Create a new Discord client and connect to the server.
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Remove the default help command.
 bot.remove_command('help')
+
+
+# ANCHOR - ping command
+@bot.command(name='ping', description='Pong!', brief='Returns pong!')
+async def _ping(ctx):
+    """
+    It sends a message to the channel the command was sent in, with a green embed that says "Pong! I'm
+    working :)"
+
+    :param ctx: The context of where the command was used
+    """
+    embed = discord.Embed(
+        title='Pong!', description='Pong! I\'m working :)', color=discord.Color.green())
+    embed.set_author(
+        name="Working :)", icon_url="https://i.imgflip.com/5uscl9.png")
+    await ctx.send(embed=embed)
 
 
 @bot.event
