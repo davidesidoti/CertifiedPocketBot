f9777bd61606a99b7f5786e8112d4b4bea851260
hash_developer <63818411+davidesidoti@users.noreply.github.com>
2022-09-04 19:38:14 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -28,12 +28,12 @@ bot.remove_command('help')
 async def _help(ctx):
     embed = discord.Embed(
         title='Help', description='List of commands', color=discord.Color.green())
-    embed.add_field(name='!help', value='Displays this message.', inline=True)
-    embed.add_field(name='!translate',
+    embed.add_field(name='*!help*', value='Displays this message.', inline=True)
+    embed.add_field(name='*!translate*',
                     value='Translates a message to a given language. [English: en; German: de; Turkish: tr; Italian: it;]', inline=True)
     embed.add_field(
-        name='!random', value='Generates a random number between two given numbers.', inline=True)
-    embed.add_field(name='!ping', value='Pong!', inline=True)
+        name='*!random*', value='Generates a random number between two given numbers.', inline=True)
+    embed.add_field(name='*!ping*', value='Pong!', inline=True)
     embed.set_author(
         name='Help', icon_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFVMaOjMavzlclHBRR7cgTgPjRNHlzUCPswlImvSeSRw&s')
     embed.set_footer(
